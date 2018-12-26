645e9d91598a7f60a2e2dc766aaf3c45b8782dc4
xb <173581251@qq.com>
2018-12-26 19:49:03 +0800

    wait push notify

--- ios/app/AppDelegate.m
+++ ios/app/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import "LWQRCodeViewController.h"
+#import <CloudPushSDK/CloudPushSDK.h>
 
 @implementation AppDelegate
 
@@ -35,7 +36,54 @@
 //  self.window.rootViewController = [[LWQRCodeViewController alloc] init];
 //  self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [self initCloudPush];
   return YES;
 }
 
+
+- (void)initCloudPush {
+  // SDK初始化
+  [CloudPushSDK asyncInit:@"25447832" appSecret:@"8d8a09172abcb60e940513fafe627f8c" callback:^(CloudPushCallbackResult *res) {
+    if (res.success) {
+      NSLog(@"Push SDK init success, deviceId: %@.", [CloudPushSDK getDeviceId]);
+    } else {
+      NSLog(@"Push SDK init failed, error: %@", res.error);
+    }
+  }];
+}
+- (void)registerAPNS:(UIApplication *)application {
+  if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0) {
+    // iOS 8 Notifications
+    [application registerUserNotificationSettings:
+     [UIUserNotificationSettings settingsForTypes:
+      (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge)
+                                       categories:nil]];
+    [application registerForRemoteNotifications];
+  }
+  else {
+    // iOS < 8 Notifications
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:
+     (UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)];
+  }
+}
+/*
+ *  苹果推送注册成功回调，将苹果返回的deviceToken上传到CloudPush服务器
+ */
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  [CloudPushSDK registerDevice:deviceToken withCallback:^(CloudPushCallbackResult *res) {
+    if (res.success) {
+      NSLog(@"upload token success");
+      NSLog(@"%@", res);
+      NSLog(@"Register deviceToken success.");
+    } else {
+      NSLog(@"Register deviceToken failed, error: %@", res.error);
+    }
+  }];
+}
+/*
+ *  苹果推送注册失败回调
+ */
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+  NSLog(@"didFailToRegisterForRemoteNotificationsWithError %@", error);
+}
 @end
